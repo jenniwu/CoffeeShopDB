e2c014bb71bfa7b2809e744e39442e0dd2abce81
shiqi fan <shiqi.fan@alumni.ubc.ca>
2017-11-18 18:36:53 -0800

    view Product and Salary is done

---
+++ src/oracleDBA/SalaryInfo.java
@@ -0,0 +1,26 @@
+package oracleDBA;
+
+public class SalaryInfo {
+    int TIER     ;
+    String  FREQUENCY ;
+    int BONUS;
+
+    public SalaryInfo(int tier , String  frequency , int bonus  ){
+
+        this.TIER = tier;
+        this.FREQUENCY = frequency;
+        this.BONUS = bonus;
+    }
+
+    public int getTIER(){
+        return TIER;
+    }
+    public String getFREQUENCY(){
+        return FREQUENCY;
+    }
+    public int getBONUS(){
+        return BONUS;
+    }
+
+}
+
